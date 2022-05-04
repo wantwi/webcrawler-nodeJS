fe24bae3a87c73a08bbb6150e422c77bfde7775a
William Antwi-Boasiako <william.antwi-boasiako@persol.net>
2022-05-04 15:43:20 +0000

    error fixed

--- crawler.js
+++ crawler.js
@@ -3,115 +3,118 @@ const cheerio = require("cheerio");
 const urlParse = require("url-parse");
 const fs = require("fs");
 
-const params= process.argv.slice(2)
+const params = process.argv.slice(2);
 
-
-const start_url =params[0]
+const start_url = params[0];
 const DEPT = params[1];
 
 const url = new urlParse(start_url);
 const baseUrl = url.protocol + "//" + url.hostname;
-const HOST = url.host
-const PROTOCOL=  url.protocol
+const HOST = url.host;
+const PROTOCOL = url.protocol;
 
-let results =[],pagesToVisit=[],pagesVisited = {};
+let results = [],
+  pagesToVisit = [],
+  pagesVisited = {};
 let numPagesVisited = -1;
 
-const crawlPage =()=>{
-   
-    if (numPagesVisited >= DEPT) {
-        console.log("Done");
-        return;
-      }
+const crawlPage = () => {
+  if (numPagesVisited >= DEPT) {
+    console.log("Done");
+    return;
+  }
 
-      //console.log({pagesToVisit})
+  //console.log({pagesToVisit})
 
-    
-      let nextPage = pagesToVisit.shift();
+  let nextPage = pagesToVisit.shift();
 
-      
-    //console.log({nextPage})
+  //console.log({nextPage})
 
-    if (nextPage in pagesVisited) {
-       //console.log("page already visited")
-        crawlPage();
-      } else {
-        //console.log("page not visited")
-        visitPage(renderUrlPath(nextPage), crawlPage);
-      }
-}
+  if (nextPage in pagesVisited) {
+    //console.log("page already visited")
+    crawlPage();
+  } else {
+    //console.log("page not visited")
+    visitPage(renderUrlPath(nextPage), crawlPage);
+  }
+};
 
+const visitPage = (urlPath, Callback) => {
+  pagesVisited[url] = true;
+  numPagesVisited++;
 
+  let newObj = {};
 
-const visitPage = (urlPath, Callback)=>{
-    pagesVisited[url] = true;
-    numPagesVisited++;
+  request(urlPath, function (error, response, body) {
+    if (response.statusCode !== 200) {
+      Callback();
+      return;
+    }
 
-    let newObj ={}
+    var $ = cheerio.load(body);
+    // const anchortagLinks = $("a")
+    //   .map((i, link) => link.attribs.href)
+    //   .get()
+    //   .map((x) => renderUrlPath(x))
+    // .filter((el) => el !== undefined)
+    // .filter((el) => el.includes(`${HOST}`));
 
-    request(urlPath, function (error, response, body) {
-       
-        if (response.statusCode !== 200) {
-            Callback();
-          return;
-        }
-       
-      
-        var $ = cheerio.load(body);
-        const anchortagLinks = $("a")
-          .map((i, link) => link.attribs.href)
-          .get().map(x => renderUrlPath(x)).filter(el => el.includes(`${HOST}`) )
-    
-          //console.log({anchortagLinks})
-    
-        const imgSrc = $("img")
-          .map((i, link) => link.attribs.src)
-          .get().map(x => renderUrlPath(x));
-    
-          newObj.imageUrl = imgSrc;
-          newObj.sourceUrl = urlPath
-          newObj.dept =numPagesVisited
-    
-        results = [...results, newObj]
-        writeToJsonFile();
+    const anchortagLinks = $("a")
+      .map((i, link) => link.attribs.href)
+      .get()
+      .map((x) => renderUrlPath(x))
+      .filter((el) => el !== undefined)
+      .filter((el) => el.includes(`${HOST}`));
 
-        //console.log('total',[...pagesToVisit, ...anchortagLinks].length)
-    
-        pagesToVisit = [...new Set([...pagesToVisit, ...anchortagLinks])] 
-        //console.log('total2',pagesToVisit.length);
+    console.log({ anchortagLinks });
 
-        if (pagesToVisit.length !== 0) {
-            Callback();
-          }
-      
-      });
+    const imgSrc = $("img")
+      .map((i, link) => link.attribs.src)
+      .get()
+      .map((x) => renderUrlPath(x))
+      .filter((el) => el !== undefined)
+      .filter((el) => el.includes(`${HOST}`));
 
+    newObj.imageUrl = imgSrc;
+    newObj.sourceUrl = urlPath;
+    newObj.dept = numPagesVisited;
 
-}
+    results = [...results, newObj];
+    writeToJsonFile();
+
+    //console.log('total',[...pagesToVisit, ...anchortagLinks].length)
+
+    pagesToVisit = [...new Set([...pagesToVisit, ...anchortagLinks])];
+    //console.log('total2',pagesToVisit.length);
+
+    if (pagesToVisit.length !== 0) {
+      Callback();
+    }
+  });
+};
 
 const renderUrlPath = (link) => {
-   
-if (link.startsWith("/") ) {
-  return `${baseUrl}${link}`;
-}else if(link.startsWith("#")){
-  return `${baseUrl}/${link}`;
-} 
-else if (link.startsWith("http")) {
-  return link;
-} 
-else if (link.startsWith("//")) {
+  // console.log(`${baseUrl}/`, link);
+  if (link.startsWith("//")) {
     return `${PROTOCOL}${link}`;
-  } 
+  } else if (link.startsWith("#")) {
+    return `${baseUrl}/${link}`;
+  } else if (link.startsWith("http")) {
+    return link;
+  } else if (link.startsWith("/")) {
+    return `${baseUrl}${link}`;
+  } else {
+    return `${baseUrl}/${link}`;
+  }
 };
 
 const writeToJsonFile = () => {
-    fs.writeFileSync("results.json", JSON.stringify({results}), (err) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-    });
-  };
-  
+  fs.writeFileSync("results.json", JSON.stringify({ results }), (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
+};
 
-  visitPage(start_url,crawlPage)
+visitPage(start_url, crawlPage);
