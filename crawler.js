fb9c221203a2aff6b46e9c8a340880dc16c7a701
William Antwi-Boasiako <william.antwi-boasiako@persol.net>
2022-05-06 16:16:23 +0000

    first commit

---
+++ crawler.js
@@ -0,0 +1,103 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const urlParse = require("url-parse");
+const fs = require("fs");
+
+
+//test url
+const start_URL =  "https://stevescooking.blogspot.com";
+
+const url = new urlParse(start_URL);
+const baseUrl = url.protocol + "//" + url.hostname;
+const HOST = url.host
+const PROTOCOL=  url.protocol
+const DEPT = 3
+let results =[],pagesToVisit=[],pagesVisited = {};
+let numPagesVisited = -1;
+
+const crawlPage =()=>{
+    let nextPage =''
+    if (numPagesVisited >= DEPT) {
+        console.log("Reached max limit of number of pages to visit.");
+        return;
+      }
+
+       nextPage = pagesToVisit.pop();
+
+      
+    visitPage(renderUrlPath(nextPage), crawlPage);
+}
+
+
+
+const visitPage = (urlPath, Callback)=>{
+    pagesVisited[url] = true;
+    numPagesVisited++;
+
+    console.log("Visiting page " + urlPath);
+
+    let newObj ={}
+
+    request(urlPath, function (error, response, body) {
+       
+        if (response.statusCode !== 200) {
+            Callback();
+          return;
+        }
+       
+      
+        var $ = cheerio.load(body);
+        const anchortagLinks = $("a")
+          .map((i, link) => link.attribs.href)
+          .get().map(x => renderUrlPath(x)).filter(el => el.includes(`${HOST}`) )
+    
+          console.log({anchortagLinks})
+    
+        const imgSrc = $("img")
+          .map((i, link) => link.attribs.src)
+          .get().map(x => renderUrlPath(x));
+    
+          newObj.imageUrl = imgSrc;
+          newObj.sourceUrl = urlPath
+          newObj.dept =numPagesVisited
+    
+        results = [...results, newObj]
+        writeToJsonFile();
+    
+        pagesToVisit =  [...pagesToVisit, ...anchortagLinks]
+
+        if (pagesToVisit.length !== 0) {
+            Callback();
+          }
+      
+      });
+
+
+}
+
+const renderUrlPath = (link) => {
+   
+if (link.startsWith("/") ) {
+  return `${baseUrl}${link}`;
+}else if(link.startsWith("#")){
+  return `${baseUrl}/${link}`;
+} 
+else if (link.startsWith("http")) {
+  return link;
+} 
+else if (link.startsWith("//")) {
+    return `${PROTOCOL}${link}`;
+  } 
+};
+
+const writeToJsonFile = () => {
+    fs.writeFileSync("results.json", JSON.stringify({results}), (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    });
+  };
+  
+
+  visitPage(start_URL,crawlPage)
